25b90acba4a563270e09444244b2e31fdbd92e86
nevfelozdeniz <nevfelo@gmail.com>
2023-09-20 18:06:58 +0300

    KartvizitApp-Started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path:'header',
+    component:HeaderComponent
+  },
+  {
+    path:'about',
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
